310fa608055aa0dfd76e91fdf197c59e97c85c6f
mupol <148651550+mupol313@users.noreply.github.com>
2024-06-22 11:45:06 +0330

    Add files via upload

---
+++ freqtrade/rpc/api_server/ui/installed/assets/BotBalance-CLyi1JE6.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-CaCvnpjp.js";import"./index-BQYgRwvm.js";import"./installCanvasRenderer-DfGSJKBX.js";import"./createSeriesDataSimply-BU2Dcv_7.js";export{o as default};
+//# sourceMappingURL=BotBalance-CLyi1JE6.js.map
---
+++ freqtrade/rpc/api_server/ui/installed/assets/LoginView-Dv4nPX9V.js
@@ -0,0 +1,2 @@
+import{_ as t,o as c,c as _,b as e,w as s,S as a,bs as r}from"./index-BQYgRwvm.js";const i={},d={class:"container"};function l(f,p){const o=r,n=a;return c(),_("div",d,[e(n,{header:"Freqtrade bot Login"},{default:s(()=>[e(o,{ref:"loginForm"},null,512)]),_:1})])}const u=t(i,[["render",l],["__scopeId","data-v-e45c9504"]]);export{u as default};
+//# sourceMappingURL=LoginView-Dv4nPX9V.js.map
